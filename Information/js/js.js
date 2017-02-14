469924ea54ffd5c4b9c1daa56ca7ae957444ebe8
Ariel Triana <ariel.triana88@gmail.com>
2017-02-14 16:15:14 -0500

    touch-ups

--- Information/js/js.js
+++ Information/js/js.js
@@ -1,23 +1,5 @@
-$().ready(function() {
-    //alert("Hola Jquery");
-    $('button').click(function() {
-        alert("button");
-
-        $("#div1").load("abc.txt", function(responseTxt, statusTxt, xhr) {
-            if (statusTxt == "success")
-                alert("External content loaded successfully!");
-            if (statusTxt == "error")
-                alert("Error: " + xhr.status + ": " + xhr.statusText);
-        });
-    });
-
-
-
-    $('.list-tap').click(function() {
-        $('.list-tap').removeClass('active');
-        var ele = $(this).attr('data-elem');
-        $(this).addClass('active');
-        $('.data').hide();
-        $('#' + ele).show();
-    });
+$().ready(function(){
+	$("#main-menu li").hover(function(){
+		$(this).toggleClass("fon");
+	});
 });
